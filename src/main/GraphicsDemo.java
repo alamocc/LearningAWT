243e5c6c5b0b137922289b2359c8b19f76c47b52
alamocc <angelmorilla2005@gmail.com>
2026-01-19 19:55:27 +0100

    feat(main): Added simple test figures using Frame class

---
+++ src/main/GraphicsDemo.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class GraphicsDemo extends Frame {
+    public GraphicsDemo() {
+
+        addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent we) {
+                System.exit(0);
+            }
+
+        });
+
+        setBackground(Color.white);
+        setForeground(Color.BLUE);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+
+        // Drawing a string
+        g.drawString("Test String", 20, 110);
+
+        // Drawing lines
+        g.drawLine(20, 40, 100, 90);
+        g.drawLine(20, 90, 100, 40);
+        g.drawLine(40, 45, 250, 80);
+
+        // Drawing rectangles
+        g.drawRect(20, 150, 60, 50);
+        g.fillRect(110, 150, 60, 50);
+        g.drawRoundRect(200, 150, 60, 50, 15, 15);
+        g.fillRoundRect(290, 150, 60, 50, 30, 40);
+
+        // Drawing elipses and circles
+        g.drawOval(20, 250, 50, 50);
+        g.fillOval(100, 250, 75, 50);
+        g.drawOval(200, 260, 100, 40);
+
+        // Drawing arcs
+        g.drawArc(20, 350, 70, 70, 0, 180);
+        g.fillArc(70, 350, 70, 70, 0, 75);
+
+        // Drawing a polygon
+        int[] xPoints = {20, 200, 20, 200, 20};
+        int[] yPoints = {450, 450, 650, 650, 450};
+        int num = 5;
+
+        g.drawPolygon(xPoints, yPoints, num);
+    }
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,15 @@
+import main.GraphicsDemo;
+
+import java.awt.*;
 
 void main() {
-    System.out.println("Hola Mundo!");
+    testGraphicsDemo();
+}
+
+private static void testGraphicsDemo() {
+    GraphicsDemo appwindow = new GraphicsDemo();
+
+    appwindow.setSize(new Dimension(370, 700));
+    appwindow.setTitle("Graphics Demonstration");
+    appwindow.setVisible(true);
 }
